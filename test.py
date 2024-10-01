82d753acf55b0773c10d7a5f405cd8e2fcfe44ef
David Scarin <davidmscarin@gmail.com>
2024-10-01 15:31:01 +0100

    some config examples

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
 import gymnasium
+import pprint
 import highway_env
 from matplotlib import pyplot as plt
 #%matplotlib inline
 
 env = gymnasium.make('intersection-v0', render_mode='rgb_array')
+
+#print config of env
+print("configuration dict of environment:")
+pprint.pprint(env.unwrapped.config)
+
+#example to change the number of lanes
+env.unwrapped.config["lanes_count"] = 2
 env.reset()
-while True:
+
+done = False
+
+while not done:
     action = env.unwrapped.action_type.actions_indexes["IDLE"] #pick action
+    print(action)
     obs, reward, done, truncated, info = env.step(action)
-    if done:
-        print("actions: ", env.unwrapped.action_space)
-        print("observations: ", obs)
-        print("reward: ", reward)
-        print("done: ", done)
-        print("truncated: ", truncated)
-        print("info: ", info)
     env.render()
+
+print("observations: ", obs)
+print("reward: ", reward)
+print("done: ", done)
+print("truncated: ", truncated)
+print("info: ", info)
 
 plt.imshow(env.render())
 plt.show()
